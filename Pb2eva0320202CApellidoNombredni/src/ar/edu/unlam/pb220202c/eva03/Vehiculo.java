fd7435a764224d3cc90975d4e5d281a5d5dce4bf
Gab <gabrielcerri.gc@gmail.com>
2021-07-06 14:13:29 -0300

    "version inicial del proyecto"

---
+++ Pb2eva0320202CApellidoNombredni/src/ar/edu/unlam/pb220202c/eva03/Vehiculo.java
@@ -0,0 +1,14 @@
+package ar.edu.unlam.pb220202c.eva03;
+
+public class Vehiculo {
+
+	//Se debe crear contructeres getters y Setters y loos que crean convenientes
+	private String Patente;
+	private Integer VelocidadActual;
+	private Integer limiteVelocidad;
+
+	
+	public void incrmentarVelocidad(Integer Velocidad) {
+		
+	}
+}
